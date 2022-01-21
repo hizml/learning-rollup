9744f7c7b614b7f9932a68617cf3c37dcd00a508
zhaoml <menglong.zhao@geekbang.com>
2022-01-21 18:20:34 +0800

    修改

---
+++ bundle.js
@@ -0,0 +1,8 @@
+(function () {
+	'use strict';
+
+	var arr = [1, 2, 3, 4];
+	console.log(arr);
+	// rollup main.js --file bundle.js --format iife
+
+})();
---
+++ main.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, 4]
+console.log(arr)
+//
+/**
+ * 对于浏览器：
+ * # compile to a <script> containing a self-executing function ('iife')
+ * rollup main.js --file bundle.js --format iife
+ *
+ * 对于 Node.js：
+ * # compile to a CommonJS module ('cjs')
+ * rollup main.js --file bundle.js --format cjs
+ */
